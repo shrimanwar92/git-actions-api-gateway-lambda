773b1ebe7dddb81e97374a5563ae5b9b08d9ea5c
Nilay Shrimanwar <shrimanwar92@gmail.com>
2023-03-29 23:52:22 +0530

    Add files via upload

---
+++ ssm.js
@@ -0,0 +1,21 @@
+async function invokeAPI() {
+    try {
+        const res = await fetch('https://unxmpqb86h.execute-api.us-east-1.amazonaws.com/dev/lambda-tooldr');
+        const json = await res.json();
+        return json;
+    } catch (e) {
+        console.log(e);
+        throw new Error(e);
+    }
+}
+
+function ping() {
+    console.log("this is just the message")
+}
+
+invokeAPI()
+    .then(result => {
+        console.log(result.body.message);
+    }).then(() => {
+        ping()
+    })
